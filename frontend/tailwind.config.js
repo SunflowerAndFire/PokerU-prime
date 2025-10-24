961d7ce9128baeb4b7dd7754aee2e47f6e7c9de0
William T <william26@Williams-MacBook-Pro.local>
2025-10-23 20:48:36 -0500

    feat: set up codebase for front & backend on macos. runios simulator successfully

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/index.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ffffff",
+        secondary: "#151312",
+        accent: "#a503c1",
+        light: {
+          100: "#d6c6ff",
+          200: "#a8b5db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+      }
+    },
   },
   plugins: [],
 }
